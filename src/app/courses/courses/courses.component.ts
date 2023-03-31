a161a1d49eba963d18cfaced22e3759679ab009d
Alice <alicehata9@gmail.com>
2023-03-31 19:16:14 -0300

    add service and http get list

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Course } from '../model/course';
+import { CoursesService } from '../services/courses.service';
 
 @Component({
   selector: 'app-courses',
@@ -7,21 +8,12 @@ import { Course } from '../model/course';
   styleUrls: ['./courses.component.scss']
 })
 export class CoursesComponent implements OnInit {
-  courses: Course[] = [
-    {
-      _id: '1',
-      name: 'Angular',
-      category: 'Front-end'
-    },
-    {
-      _id: '2',
-      name: 'React',
-      category: 'Front-end'
-    }
-  ];
+  courses: Course[] = [];
   displayedColumns: string[] = ['name', 'category'];
 
-  constructor(){}
+  constructor(private coursesService: CoursesService){
+    this.courses = this.coursesService.list();
+  }
 
   ngOnInit() : void {}
 }
