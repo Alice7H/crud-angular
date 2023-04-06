33d22b9d2673eddf1c5c6430529ab3d7b04859e4
Alice <alicehata9@gmail.com>
2023-04-06 17:11:12 -0300

    add course form component and redirecting page

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -4,6 +4,7 @@ import { CoursesService } from '../services/courses.service';
 import { Observable, catchError, of } from 'rxjs';
 import { MatDialog } from '@angular/material/dialog';
 import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/error-dialog.component';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -12,11 +13,13 @@ import { ErrorDialogComponent } from 'src/app/shared/components/error-dialog/err
 })
 export class CoursesComponent implements OnInit {
   courses$: Observable<Course[]>;
-  displayedColumns: string[] = ['name', 'category'];
+  displayedColumns: string[] = ['name', 'category', 'actions'];
 
   constructor(
     private coursesService: CoursesService,
-    public dialog: MatDialog
+    public dialog: MatDialog,
+    private router: Router,
+    private route: ActivatedRoute
     ){
     this.courses$ = this.coursesService.list()
     .pipe(
@@ -33,5 +36,10 @@ export class CoursesComponent implements OnInit {
     })
   }
 
+  onAdd() {
+    this.router.navigate(['new'], {relativeTo: this.route});
+  }
+
   ngOnInit() : void {}
+
 }
