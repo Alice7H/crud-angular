d07f3ab526757caeb7286ea60f11b609fa0bdd1d
Alice <alicehata9@gmail.com>
2023-04-06 20:15:31 -0300

    add snackbar to show messages and back page

--- src/app/courses/course-form/course-form.component.ts
+++ src/app/courses/course-form/course-form.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
+import { Location } from '@angular/common';
 import { CoursesService } from '../services/courses.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
 
@@ -15,6 +16,7 @@ export class CourseFormComponent {
     private formBuilder : FormBuilder,
     private service: CoursesService,
     private snackBar: MatSnackBar,
+    private location: Location
     ) {
     this.form = this.formBuilder.group({
       name: [null],
@@ -25,16 +27,22 @@ export class CourseFormComponent {
   onSubmit(){
     this.service.save(this.form.value)
       .subscribe({
-        next: (result) => console.log(result),
+        next: () => this.onSuccess(),
         error: () => this.onError()
       });
+  }
+
+  onCancel(){
+    this.location.back();
   }
 
   private onError() {
     this.snackBar.open('Erro ao salvar curso', '', { duration: 3000});
   }
 
-  onCancel(){
-
+  private onSuccess() {
+    this.snackBar.open('Curso salvo com sucesso!', '', { duration: 3000});
+    this.onCancel();
   }
+
 }
