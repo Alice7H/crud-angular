88bbebb6a58070ae490706e17f8bfc1eb90a46b0
Alice <alicehata9@gmail.com>
2023-11-18 01:57:46 -0300

    add FormArray validations

--- src/app/courses/containers/course-form/course-form.component.ts
+++ src/app/courses/containers/course-form/course-form.component.ts
@@ -33,7 +33,7 @@ export class CourseFormComponent implements OnInit {
         Validators.maxLength(100)]
       ],
       category: [course.category, [Validators.required]],
-      lessons: this.formBuilder.array(this.retrieveLessons(course))
+      lessons: this.formBuilder.array(this.retrieveLessons(course), Validators.required)
     });
     console.log(this.form);
     console.log(this.form.value);
@@ -52,8 +52,16 @@ export class CourseFormComponent implements OnInit {
   private createLesson(lesson: Lesson = {id: '', name: '', youtubeUrl: ''}){
     return this.formBuilder.group({
       id: [lesson.id],
-      name: [lesson.name],
-      youtubeUrl: [lesson.youtubeUrl]
+      name: [lesson.name, [
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(100)]
+      ],
+      youtubeUrl: [lesson.youtubeUrl, [
+        Validators.required,
+        Validators.minLength(10),
+        Validators.maxLength(11)]
+      ]
     })
   }
 
@@ -72,8 +80,12 @@ export class CourseFormComponent implements OnInit {
   }
 
   onSubmit(){
-    this.service.save(this.form.value)
-      .subscribe({ next: () => this.onSuccess(), error: () => this.onError() });
+    if(this.form.valid){
+      this.service.save(this.form.value)
+        .subscribe({ next: () => this.onSuccess(), error: () => this.onError() });
+    }else {
+      alert('Invalid form submitted');
+    }
   }
 
   onCancel(){
@@ -103,5 +115,10 @@ export class CourseFormComponent implements OnInit {
       return `Este campo deve ter no máximo ${requiredLength} caracteres`;
     }
     return "Campo inválido";
+  }
+
+  isFormArrayRequired() {
+    const lessons = this.form.get('lessons') as UntypedFormArray;
+    return !lessons.valid && lessons.hasError('required') && lessons.touched;
   }
 }
